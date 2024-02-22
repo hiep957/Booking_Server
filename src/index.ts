4815350e9ae6f3930a0222758673350974ea2fef
Your Name <you@example.com>
2024-02-22 10:08:24 +0700

    y

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,18 @@ import express,{Request,Response} from 'express';
 import cors from 'cors';
 import "dotenv/config"
 import mongoose from 'mongoose';
-import userRouter from './routes/users'
-import authRouter from './routes/auth'
+import userRoutes from './routes/users'
+import authRoutes from './routes/auth'
+import myhotelsRoutes from './routes/my-hotels'
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from 'cloudinary';
           
-cloudinary.config({ 
-  cloud_name: 'dmlfolzmj', 
-  api_key: '938816921467175', 
-  api_secret: 'zmTMF4dT2XRseybZEjSVY1_DKAI' 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 mongoose.connect(process.env.MONGO_URL as string);
 const app = express();
 app.use(cookieParser());
@@ -28,8 +30,9 @@ app.get("/api/test",async (req:Request,res:Response)=>{
     res.json({message: "Hello from express endpoint!"});
 })
 
-app.use("/api/users",userRouter);
-app.use("/api/auth",authRouter);
+app.use("/api/users",userRoutes);
+app.use("/api/auth",authRoutes);
+app.use("api/my-hotels", myhotelsRoutes)
 
 app.listen(7000,()=>{
     console.log("server running on localhost:7000");
