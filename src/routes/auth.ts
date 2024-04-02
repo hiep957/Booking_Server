525b90cc27d2799590b45210dc27a5e7781b613a
hiep957 <hicharmie1@gmail.com>
2024-04-02 17:42:21 +0700

    init

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -52,7 +52,7 @@ router.post(
             res.status(200).json({ userId: user._id });
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: "Something went wrong" });
+            res.status(500).json({ message: "Something went wronsssg" });
         }
     }
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,59 +3,57 @@ import User from "../models/user";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
 const router = express.Router();
-import "dotenv/config"
+import "dotenv/config";
 router.post(
-    "/register",
+  "/register",
 
-    [
-        check("firstName", "First Name is required").isString(),
-        check("lastName", "Last Name is required").isString(),
-        check("email", "Email is required").isEmail(),
-        check("password", "Password with 6 or more characters required").isLength({
-            min: 6,
-        }),
-    ],
-    
-    async (req:Request, res:Response) => {
+  [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
 
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ message: errors.array() });
-        }
-    try {
-        let user = await User.findOne({
-            email:req.body.email
-        })
-
-        if(user) {
-            return res.status(400).json({message:"User already exists"});
-        }
-        
-        user = new User(req.body);
-        await user.save();
-
-        const token = jwt.sign(
-            {
-            userId: user.id,
-            },
-            process.env.JWT_SECRET as string,
-            {
-                expiresIn: "1d"
-            }
-
-        );
-
-        res.cookie("auth_token", token, {
-            httpOnly: true,
-            secure: false,
-            maxAge: 86400000,
-          });
-        return res.status(200).send({ message: "User registered OK" });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({message:"Something went wrong"})
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
     }
-})
+    try {
+      let user = await User.findOne({
+        email: req.body.email,
+      });
 
+      if (user) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+
+      user = new User(req.body);
+      await user.save();
+
+      const token = jwt.sign(
+        {
+          userId: user.id,
+        },
+        process.env.JWT_SECRET as string,
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: false,
+        maxAge: 86400000,
+      });
+      return res.status(200).send({ message: "User registered OK" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
 
 export default router;
